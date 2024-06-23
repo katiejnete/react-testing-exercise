01f7fb2347ea043d61cc7f4d5e82fe7d30df94d7
Kate Vong <katiejnete@gmail.com>
2024-06-23 00:06:32 -0700

    update bug fix

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,60 +2,49 @@ import { useState } from "react";
 import "./Carousel.css";
 import Card from "./Card";
 
-
 /** Carousel: displays images and arrows to navigate through them
- * 
+ *
  * Props:
  * - photos: array of {src, caption} objects
  * - title: string describing the collection of images
- * 
+ *
  * State:
  * - currCardIdx: integer for current card index
- * 
+ *
  * App --> Carousel --> Card
  */
- function Carousel({ photos, title }) {
+function Carousel({ photos, title }) {
   const [currCardIdx, setCurrCardIdx] = useState(0);
 
   const currCard = photos[currCardIdx];
   const total = photos.length;
+  const max = currCardIdx === total - 1;
+  const min = currCardIdx === 0;
 
   //Increments currCardIdx state by 1
   function goForward() {
-    if (currCardIdx === total - 1) {
-      setCurrCardIdx(0);
-    } else {
-      setCurrCardIdx(currCardIdx + 1);
-    }
+    if (currCardIdx === total - 1) return;
+    setCurrCardIdx(currCardIdx + 1);
   }
 
   //Subtracts currCardIdx state by 1
   function goBackward() {
-    if (currCardIdx === 0) {
-      setCurrCardIdx( total - 1);
-    } else {
-      setCurrCardIdx(currCardIdx - 1);
-    }
+    if (currCardIdx === 0) return;
+    setCurrCardIdx(currCardIdx - 1);
   }
 
   return (
     <div className="Carousel">
       <h1>{title}</h1>
       <div className="Carousel-main">
-        <i
-          className="bi bi-arrow-left-circle"
-          onClick={goBackward}
-        />
+        {!min && <i className="bi bi-arrow-left-circle" onClick={goBackward} />}
         <Card
           caption={currCard.caption}
           src={currCard.src}
           currNum={currCardIdx + 1}
           totalNum={total}
         />
-        <i
-          className="bi bi-arrow-right-circle"
-          onClick={goForward}
-        />
+        {!max && <i className="bi bi-arrow-right-circle" onClick={goForward} />}
       </div>
     </div>
   );
